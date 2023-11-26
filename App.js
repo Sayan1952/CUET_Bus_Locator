7fdbbf0d5d276acf31a0d22dbcccdf86d0ce7b5c
Sayan1952 <sprontu@gmail.com>
2023-11-26 22:59:52 +0600

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
-export default function App() {
+const App = () => {
+  const [busLocation, setBusLocation] = useState({
+    latitude: 22.3475, // Replace with your initial bus latitude
+    longitude: 91.8123, // Replace with your initial bus longitude
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const mapRef = useRef(null); // Ref for MapView
+
+  const animateToBus = () => {
+    if (mapRef.current) {
+      mapRef.current.animateToRegion(busLocation, 1000); // Animating to bus location with a duration of 1000ms
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        ref={mapRef} // Assigning the ref to the MapView
+        style={styles.map}
+        initialRegion={busLocation}
+      >
+        <Marker
+          coordinate={{
+            latitude: busLocation.latitude,
+            longitude: busLocation.longitude,
+          }}
+          title="Bus Marker"
+        />
+      </MapView>
+      <TouchableOpacity style={styles.button} onPress={animateToBus}>
+        <Text>Find Bus</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    flex: 1,
+  },
+  button: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: 'lightblue',
+    padding: 10,
+    borderRadius: 10,
   },
 });
+
+export default App;
